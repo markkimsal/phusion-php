0a4c14a175ddc73d8affb9acba5007e68ca03ede
Mark Kimsal <metrofindings@gmail.com>
2021-06-12 07:04:52 -0400

    re-arrange php 8 docker files

--- make-80.sh
+++ make-80.sh
@@ -1,3 +1,3 @@
-#docker build -t markkimsal/php:8.0.7-fpm --target=fpm -f ./php-80/Dockerfile-fpm php-80
-#docker build -t markkimsal/phusion-php:8.0.7-fpm -f ./php-80/Dockerfile-fpm php-80
+docker build -t markkimsal/php:8.0.7-fpm --target=fpm -f ./php-80/Dockerfile-php php-80
+docker build -t markkimsal/phusion-php:8.0.7-fpm -f ./php-80/Dockerfile-phusion php-80
 docker build -t markkimsal/phusion-php:8.0.7-builder -f ./php-80/Dockerfile-builder php-80
