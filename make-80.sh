78f9debf80e39ccf02e0e9930bfd597dd5b962c8
Mark Kimsal <metrofindings@gmail.com>
2021-08-28 10:01:28 -0400

    add more tags removing the point release

--- make-80.sh
+++ make-80.sh
@@ -1,3 +1,8 @@
-docker build -t markkimsal/php:8.0.7-fpm --target=fpm -f ./php-80/Dockerfile-php php-80
-docker build -t markkimsal/phusion-php:8.0.7-fpm -f ./php-80/Dockerfile-phusion php-80
-docker build -t markkimsal/phusion-php:8.0.7-builder -f ./php-80/Dockerfile-builder php-80
+
+docker build -t markkimsal/php:8.0.10-fpm --target=fpm -f ./php-80/Dockerfile-php php-80
+docker build -t markkimsal/phusion-php:8.0.10-fpm -f ./php-80/Dockerfile-phusion php-80
+docker build -t markkimsal/phusion-php:8.0.10-builder -f ./php-80/Dockerfile-builder php-80
+
+docker tag markkimsal/phusion-php:8.0.10-builder  markkimsal/phusion-php:8.0-builder
+docker tag markkimsal/phusion-php:8.0.10-fpm  markkimsal/phusion-php:8.0-fpm
+docker tag markkimsal/phusion-php:8.0.10-fpm  markkimsal/phusion-php:latest
