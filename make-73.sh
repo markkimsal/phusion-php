75c512730d068efaafd749b54e34201ff49d7894
Mark Kimsal <metrofindings@gmail.com>
2021-08-28 11:05:59 -0400

    update: php-7.3.30

--- make-73.sh
+++ make-73.sh
@@ -1,4 +1,9 @@
-docker build -t markkimsal/phusion-lemp:7.3.26-mssql -f ./Dockerfile-mssql . 
-docker build -t markkimsal/phusion-lemp:7.3.26-pgsql -f ./Dockerfile-pgsql . 
-docker build -t markkimsal/phusion-lemp:7.3.26-mysql -f ./Dockerfile-mysql . 
-docker build -t markkimsal/phusion-lemp:7.3.26-builder -f ./Dockerfile-builder . 
+docker build -t markkimsal/phusion-lemp:7.3.30-mssql -f ./Dockerfile-mssql .
+docker build -t markkimsal/phusion-lemp:7.3.30-pgsql -f ./Dockerfile-pgsql .
+docker build -t markkimsal/phusion-lemp:7.3.30-mysql -f ./Dockerfile-mysql .
+docker build -t markkimsal/phusion-lemp:7.3.30-builder -f ./Dockerfile-builder .
+
+docker tag markkimsal/phusion-lemp:7.3.30-builder markkimsal/phusion-lemp:7.3-builder
+docker tag markkimsal/phusion-lemp:7.3.30-mysql markkimsal/phusion-lemp:7.3-mysql
+docker tag markkimsal/phusion-lemp:7.3.30-pgsql markkimsal/phusion-lemp:7.3-pgsql
+docker tag markkimsal/phusion-lemp:7.3.30-mssql markkimsal/phusion-lemp:7.3-mssql
