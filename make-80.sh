369f9c54118665748bda371c6fed2bdfc833ba98
Mark Kimsal <metrofindings@gmail.com>
2021-11-26 08:13:08 -0500

    feat: grpc for php 8

--- make-80.sh
+++ make-80.sh
@@ -2,6 +2,7 @@
 TENSOR="${TENSOR:-}"
 SAMBA="${SAMBA:-}"
 SQLSRV="${SQLSRV:-}"
+GRPC="${GRPC:-}"
 
 IMAGE_NAME=phusion-php
 if [ ! -z "$TENSOR" ]; then
@@ -13,6 +14,9 @@ fi
 if [ ! -z "$SQLSRV" ]; then
 	IMAGE_NAME="${IMAGE_NAME}-mssql"
 fi
+if [ ! -z "$GRPC" ]; then
+	IMAGE_NAME="${IMAGE_NAME}-grpc"
+fi
 
 echo "Building tag markkimsal/$IMAGE_NAME ..."
 
@@ -20,6 +24,7 @@ docker build -t markkimsal/php:8.0.10-fpm \
 	--build-arg WITH_TENSOR="${TENSOR}" \
 	--build-arg WITH_SAMBA="${SAMBA}" \
 	--build-arg WITH_SQLSRV="${SQLSRV}" \
+	--build-arg WITH_GRPC="${GRPC}" \
 	--target=fpm \
 	-f ./php-80/Dockerfile-php php-80
 
@@ -27,18 +32,20 @@ docker build -t markkimsal/$IMAGE_NAME:8.0.10-fpm \
 	--build-arg WITH_TENSOR="${TENSOR}" \
 	--build-arg WITH_SAMBA="${SAMBA}" \
 	--build-arg WITH_SQLSRV="${SQLSRV}" \
+	--build-arg WITH_GRPC="${GRPC}" \
 	-f ./php-80/Dockerfile-phusion php-80
 
 docker build -t markkimsal/$IMAGE_NAME:8.0.10-builder \
 	--build-arg WITH_TENSOR="${TENSOR}" \
 	--build-arg WITH_SAMBA="${SAMBA}" \
 	--build-arg WITH_SQLSRV="${SQLSRV}" \
+	--build-arg WITH_GRPC="${GRPC}" \
 	-f ./php-80/Dockerfile-builder php-80
 
 docker tag markkimsal/$IMAGE_NAME:8.0.10-builder  markkimsal/$IMAGE_NAME:8.0-builder
 docker tag markkimsal/$IMAGE_NAME:8.0.10-fpm  markkimsal/$IMAGE_NAME:8.0-fpm
 docker tag markkimsal/$IMAGE_NAME:8.0.10-fpm  markkimsal$IMAGE_NAMEp:latest
 
-docker push markkimsal/$IMAGE_NAME:8.0.10-builder; docker push markkimsal/$IMAGE_NAME:8.0-builder
+# docker push markkimsal/$IMAGE_NAME:8.0.10-builder; docker push markkimsal/$IMAGE_NAME:8.0-builder
 docker push markkimsal/$IMAGE_NAME:8.0.10-fpm;     docker push markkimsal/$IMAGE_NAME:8.0-fpm
 docker push markkimsal/$IMAGE_NAME:8.0.10-fpm;     docker push markkimsal/$IMAGE_NAME:latest
