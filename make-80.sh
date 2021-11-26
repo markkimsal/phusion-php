edb160a0f908f6d23638c8e500835b61c31e6c7b
Mark Kimsal <metrofindings@gmail.com>
2021-11-26 08:21:25 -0500

    update php 80 to 8.0.13

--- make-80.sh
+++ make-80.sh
@@ -20,7 +20,7 @@ fi
 
 echo "Building tag markkimsal/$IMAGE_NAME ..."
 
-docker build -t markkimsal/php:8.0.10-fpm \
+docker build -t markkimsal/php:8.0.13-fpm \
 	--build-arg WITH_TENSOR="${TENSOR}" \
 	--build-arg WITH_SAMBA="${SAMBA}" \
 	--build-arg WITH_SQLSRV="${SQLSRV}" \
@@ -28,24 +28,24 @@ docker build -t markkimsal/php:8.0.10-fpm \
 	--target=fpm \
 	-f ./php-80/Dockerfile-php php-80
 
-docker build -t markkimsal/$IMAGE_NAME:8.0.10-fpm \
+docker build -t markkimsal/$IMAGE_NAME:8.0.13-fpm \
 	--build-arg WITH_TENSOR="${TENSOR}" \
 	--build-arg WITH_SAMBA="${SAMBA}" \
 	--build-arg WITH_SQLSRV="${SQLSRV}" \
 	--build-arg WITH_GRPC="${GRPC}" \
 	-f ./php-80/Dockerfile-phusion php-80
 
-docker build -t markkimsal/$IMAGE_NAME:8.0.10-builder \
+docker build -t markkimsal/$IMAGE_NAME:8.0.13-builder \
 	--build-arg WITH_TENSOR="${TENSOR}" \
 	--build-arg WITH_SAMBA="${SAMBA}" \
 	--build-arg WITH_SQLSRV="${SQLSRV}" \
 	--build-arg WITH_GRPC="${GRPC}" \
 	-f ./php-80/Dockerfile-builder php-80
 
-docker tag markkimsal/$IMAGE_NAME:8.0.10-builder  markkimsal/$IMAGE_NAME:8.0-builder
-docker tag markkimsal/$IMAGE_NAME:8.0.10-fpm  markkimsal/$IMAGE_NAME:8.0-fpm
-docker tag markkimsal/$IMAGE_NAME:8.0.10-fpm  markkimsal$IMAGE_NAMEp:latest
+docker tag markkimsal/$IMAGE_NAME:8.0.13-builder  markkimsal/$IMAGE_NAME:8.0-builder
+docker tag markkimsal/$IMAGE_NAME:8.0.13-fpm  markkimsal/$IMAGE_NAME:8.0-fpm
+docker tag markkimsal/$IMAGE_NAME:8.0.13-fpm  markkimsal$IMAGE_NAMEp:latest
 
-# docker push markkimsal/$IMAGE_NAME:8.0.10-builder; docker push markkimsal/$IMAGE_NAME:8.0-builder
-docker push markkimsal/$IMAGE_NAME:8.0.10-fpm;     docker push markkimsal/$IMAGE_NAME:8.0-fpm
-docker push markkimsal/$IMAGE_NAME:8.0.10-fpm;     docker push markkimsal/$IMAGE_NAME:latest
+docker push markkimsal/$IMAGE_NAME:8.0.13-builder; docker push markkimsal/$IMAGE_NAME:8.0-builder
+docker push markkimsal/$IMAGE_NAME:8.0.13-fpm;     docker push markkimsal/$IMAGE_NAME:8.0-fpm
+docker push markkimsal/$IMAGE_NAME:latest
