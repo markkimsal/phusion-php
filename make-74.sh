29b72773f9d0b514ea41e6f83356a744f803fad1
Mark Kimsal <metrofindings@gmail.com>
2021-11-27 09:06:12 -0500

    upgrade to 7.4.26

--- make-74.sh
+++ make-74.sh
@@ -16,27 +16,27 @@ fi
 
 echo "Building tag markkimsal/$IMAGE_NAME ..."
 
-docker build -t markkimsal/php:7.4.23-fpm \
+docker build -t markkimsal/php:7.4.26-fpm \
 	--build-arg WITH_TENSOR="${TENSOR}" \
 	--build-arg WITH_SAMBA="${SAMBA}" \
 	--build-arg WITH_SQLSRV="${SQLSRV}" \
 	--target=fpm \
 	-f ./php-74/Dockerfile-php php-74
 
-docker build -t markkimsal/${IMAGE_NAME}:7.4.23-fpm \
+docker build -t markkimsal/${IMAGE_NAME}:7.4.26-fpm \
 	--build-arg WITH_TENSOR="${TENSOR}" \
 	--build-arg WITH_SAMBA="${SAMBA}" \
 	--build-arg WITH_SQLSRV="${SQLSRV}" \
 	-f ./php-74/Dockerfile-phusion php-74
 
-docker build -t markkimsal/${IMAGE_NAME}:7.4.23-builder \
+docker build -t markkimsal/${IMAGE_NAME}:7.4.26-builder \
 	--build-arg WITH_TENSOR="${TENSOR}" \
 	--build-arg WITH_SAMBA="${SAMBA}" \
 	--build-arg WITH_SQLSRV="${SQLSRV}" \
 	-f ./php-74/Dockerfile-builder php-74
 
-docker tag markkimsal/${IMAGE_NAME}:7.4.23-builder  markkimsal/${IMAGE_NAME}:7.4-builder
-#docker tag markkimsal/${IMAGE_NAME}:7.4.23-fpm  markkimsal/${IMAGE_NAME}:7.4-fpm
+docker tag markkimsal/${IMAGE_NAME}:7.4.26-builder  markkimsal/${IMAGE_NAME}:7.4-builder
+#docker tag markkimsal/${IMAGE_NAME}:7.4.26-fpm  markkimsal/${IMAGE_NAME}:7.4-fpm
 
-docker push markkimsal/${IMAGE_NAME}:7.4.23-builder; docker push markkimsal/${IMAGE_NAME}:7.4-builder
-docker push markkimsal/${IMAGE_NAME}:7.4.23-fpm;     docker push markkimsal/${IMAGE_NAME}:7.4-fpm
+docker push markkimsal/${IMAGE_NAME}:7.4.26-builder; docker push markkimsal/${IMAGE_NAME}:7.4-builder
+docker push markkimsal/${IMAGE_NAME}:7.4.26-fpm;     docker push markkimsal/${IMAGE_NAME}:7.4-fpm
