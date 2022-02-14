e4e21fb611a37c15521b1a5e87c58fad9a0b571e
Mark Kimsal <metrofindings@gmail.com>
2022-02-14 11:05:48 -0500

    feat: add image magick extension to php80

--- make-80.sh
+++ make-80.sh
@@ -3,6 +3,8 @@ TENSOR="${TENSOR:-}"
 SAMBA="${SAMBA:-}"
 SQLSRV="${SQLSRV:-}"
 GRPC="${GRPC:-}"
+IMAGICK="${IMAGICK:-}"
+GMAGICK="${GMAGICK:-}"
 
 IMAGE_NAME=phusion-php
 if [ ! -z "$TENSOR" ]; then
@@ -17,6 +19,13 @@ fi
 if [ ! -z "$GRPC" ]; then
 	IMAGE_NAME="${IMAGE_NAME}-grpc"
 fi
+if [ ! -z "$IMAGICK" ]; then
+	IMAGE_NAME="${IMAGE_NAME}-imagick"
+fi
+if [ ! -z "$GMAGICK" ]; then
+	IMAGE_NAME="${IMAGE_NAME}-gmagick"
+fi
+
 
 echo "Building tag markkimsal/$IMAGE_NAME ..."
 
@@ -25,6 +34,8 @@ echo "docker build -t markkimsal/php:8.0.15-fpm \
 	--build-arg WITH_SAMBA="${SAMBA}" \
 	--build-arg WITH_SQLSRV="${SQLSRV}" \
 	--build-arg WITH_GRPC="${GRPC}" \
+	--build-arg WITH_IMAGICK="${IMAGICK}" \
+	--build-arg WITH_GMAGICK="${GMAGICK}" \
 	--target=fpm \
 	-f ./php-80/Dockerfile-php php-80"
 
@@ -34,6 +45,8 @@ docker build -t markkimsal/php:8.0.15-fpm \
 	--build-arg WITH_SAMBA="${SAMBA}" \
 	--build-arg WITH_SQLSRV="${SQLSRV}" \
 	--build-arg WITH_GRPC="${GRPC}" \
+	--build-arg WITH_IMAGICK="${IMAGICK}" \
+	--build-arg WITH_GMAGICK="${GMAGICK}" \
 	--target=fpm \
 	-f ./php-80/Dockerfile-php php-80
 
@@ -42,6 +55,8 @@ docker build -t markkimsal/$IMAGE_NAME:8.0.15-fpm \
 	--build-arg WITH_SAMBA="${SAMBA}" \
 	--build-arg WITH_SQLSRV="${SQLSRV}" \
 	--build-arg WITH_GRPC="${GRPC}" \
+	--build-arg WITH_IMAGICK="${IMAGICK}" \
+	--build-arg WITH_GMAGICK="${GMAGICK}" \
 	-f ./php-80/Dockerfile-phusion php-80
 
 docker build -t markkimsal/$IMAGE_NAME:8.0.15-builder \
@@ -49,12 +64,14 @@ docker build -t markkimsal/$IMAGE_NAME:8.0.15-builder \
 	--build-arg WITH_SAMBA="${SAMBA}" \
 	--build-arg WITH_SQLSRV="${SQLSRV}" \
 	--build-arg WITH_GRPC="${GRPC}" \
+	--build-arg WITH_IMAGICK="${IMAGICK}" \
+	--build-arg WITH_GMAGICK="${GMAGICK}" \
 	-f ./php-80/Dockerfile-builder php-80
 
-docker tag markkimsal/$IMAGE_NAME:8.0.13-builder  markkimsal/$IMAGE_NAME:8.0-builder
-docker tag markkimsal/$IMAGE_NAME:8.0.13-fpm  markkimsal/$IMAGE_NAME:8.0-fpm
-docker tag markkimsal/$IMAGE_NAME:8.0.13-fpm  markkimsal$IMAGE_NAMEp:latest
+docker tag markkimsal/$IMAGE_NAME:8.0.15-builder  markkimsal/$IMAGE_NAME:8.0-builder
+docker tag markkimsal/$IMAGE_NAME:8.0.15-fpm  markkimsal/$IMAGE_NAME:8.0-fpm
+docker tag markkimsal/$IMAGE_NAME:8.0.15-fpm  markkimsal$IMAGE_NAMEp:latest
 
-docker push markkimsal/$IMAGE_NAME:8.0.13-builder; docker push markkimsal/$IMAGE_NAME:8.0-builder
-docker push markkimsal/$IMAGE_NAME:8.0.13-fpm;     docker push markkimsal/$IMAGE_NAME:8.0-fpm
+docker push markkimsal/$IMAGE_NAME:8.0.15-builder; docker push markkimsal/$IMAGE_NAME:8.0-builder
+docker push markkimsal/$IMAGE_NAME:8.0.15-fpm;     docker push markkimsal/$IMAGE_NAME:8.0-fpm
 docker push markkimsal/$IMAGE_NAME:latest
